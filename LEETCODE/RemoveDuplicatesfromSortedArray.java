d2ed282eca67a3b66356ddc6b546ac2e00d98d34
Emma Paris <eparis@atlassian.com>
2023-03-18 13:33:42 +0530

    Leetcode 26. RemoveDuplicatesfromSortedArray

---
+++ LEETCODE/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class RemoveDuplicatesfromSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System);
+        int nums = sc.nextInt();
+        // removeDuplicates(nums);
+
+    }
+
+   
+        public int removeDuplicates(int[] nums) {
+            int j = 0;
+            for(int i = 0;i<nums.length;i++){
+                if(nums[j]!=nums[i]){
+                    j++;
+                nums[j]=nums[i];
+                }
+            }
+            return j+1;
+        }
+    }
+
